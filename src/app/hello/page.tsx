bbac15a4dba2ffc1b9419e9486ef353f6ee996ed
yuto ichinose <yuto.0526.sano@gmail.com>
2023-10-17 23:10:53 +0900

    add: コンポーネント作成

---
+++ src/app/hello/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next"
+
+const Hello:NextPage = () => {
+    return <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Hello World</h1>
+    </main>
+}
+
+export default Hello
